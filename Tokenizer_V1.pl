aaac1c6a84c9b1653f98f23c4e99b56704e7da78
Harshita Kajal <37720802+HarshitaKAM@users.noreply.github.com>
2018-04-26 10:02:20 -0700

    Update Tokenizer_V1.pl

--- Tokenizer_V1.pl
+++ Tokenizer_V1.pl
@@ -1,29 +1,38 @@
-% Tokenizer_v1 (tokenizes every single string from the input file) : NEEDS Changes
-% Author- Harshita Kajal
+            % Tokenizer_v1 (tokenizes every single string from the input file) : NEEDS Changes
+            % Author- Harshita Kajal
 
-%generates list of tokens taking file as input. Sample running code is: tokenize("document.txt", List).
-
-tokenize(Input_fileName, ListOf_Tokens) :-
- 							 open(Input_fileName, read, Stream),
- 							 readfrom(Stream, FileTokens),
- 							 close(Stream).
-							
+%generates list of tokens taking file as input. Sample running code is: ?- tokenize("document.txt", List).
 
 
 %adding tokens to list
+
+
 
 readfrom(Stream, [Token|FileTokens]) :-
  							 \+ at_end_of_stream(Stream),
   							findNext(Stream, Token),
  							 !,
  							 readfrom(Stream, FileTokens).
+
+
 readfrom(_, []).
 
 
 
 
-charNext(Stream, [Ascii_Char|String], Ascii) :-
- 							
+generate([], []) :-
+  !.
+generate([''|FileTokens], ListOf_Tokens) :-
+ 								 !,
+ 								 generate(FileTokens, ListOf_Tokens).
+generate([Token|FileTokens], [Token|ListOf_Tokens]) :-
+generate(FileTokens, ListOf_Tokens).
+
+
+
+
+charNext(Stream, [Ascii_Char|String], Ascii_Char) :-
+                             Ascii_Char > 32, %&& Ascii_Char <47 ,
  							 get0(Stream, AsciiChar_Next),
  							 !,
  							 charNext(Stream, String, AsciiChar_Next).
@@ -35,5 +44,18 @@ charNext(_, [], _).
 findNext(Stream, Token) :-
 							  get0(Stream, Ascii_Char),
  							 charNext(Stream, String, Ascii_Char),
-							  atom_number(Token, String),
+							  %atom_number(Token, String),
 							  atom_codes(Token, String).
+							  %number_codes(Token, String).
+
+
+
+
+
+tokenize(Input_fileName, ListOf_Tokens) :-
+ 							 open(Input_fileName, read, Stream),
+ 							 readfrom(Stream, FileTokens),
+ 							 close(Stream),
+							generate(FileTokens, ListOf_Tokens).
+
+
